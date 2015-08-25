5c12bb5f9ff19958f164e36270e8905608909084
Boris Hruruguen <boris.huruguen@gmail.com>
2015-08-25 15:46:59 +0200

    liens et validations

--- app/models/dish.rb
+++ app/models/dish.rb
@@ -1,3 +1,9 @@
 class Dish < ActiveRecord::Base
   belongs_to :user
+
+  has_many :ingredients
+  has_many :portions
+  has_many :orders, through: :portions
+
+  validates :name, presence: true
 end
