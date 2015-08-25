8fb28fae9c43e91ac516183f4debd1a32d364306
Valerie Chatelet <vlrchtlt@gmail.com>
2015-08-25 11:55:56 +0200

    fb auth implemented for localhost

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -259,4 +259,7 @@ Devise.setup do |config|
   # When using OmniAuth, Devise cannot automatically set OmniAuth path,
   # so you need to do it manually. For the users scope, it would be:
   # config.omniauth_path_prefix = '/my_engine/users/auth'
+
+  # config facebook
+  config.omniauth :facebook, ENV["FB_ID"], ENV["FB_SECRET"], scope: 'public_profile, email', info_fields: 'email, name', image_size: 'large'
 end
