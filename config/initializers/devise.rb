61109b14a55126ab46357af1579f1e76519516fe
Valerie Chatelet <vlrchtlt@gmail.com>
2015-08-25 12:41:11 +0200

    facebook connect - pb non résolu URL is not allowed by the Application configuration

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -261,5 +261,5 @@ Devise.setup do |config|
   # config.omniauth_path_prefix = '/my_engine/users/auth'
 
   # config facebook
-  config.omniauth :facebook, ENV["FB_ID"], ENV["FB_SECRET"], scope: 'public_profile, email', info_fields: 'email, name', image_size: 'large'
+  config.omniauth :facebook, ENV["FB_ID"], ENV["FB_SECRET"], scope: 'email', info_fields: 'email,first_name,last_name', image_size: 'large'
 end
