a9dc88af8be2a4ca0fe12db9554238746639aeb2
Valerie <vlrchtlt@gmail.com>
2015-08-28 11:33:11 +0200

    Update dish.rb

--- app/models/dish.rb
+++ app/models/dish.rb
@@ -6,7 +6,7 @@ class Dish < ActiveRecord::Base
   has_many :orders, through: :portions
 
   has_attached_file :picture,
-    styles: { large: "800x800>", medium: "300x300>", thumb: "100x100>" },
+    styles: { large: "800x800>", medium: "300x300>", thumb: "100x100>" }
 
   validates :name, presence: true
 
